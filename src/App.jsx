28d2d5bab9fab2e96f810ac731f29c1784048415
Chinmoy Mandal <chinmoymandal925807@gmail.com>
2025-04-29 20:55:51 +0530

    add functin of newGame  button

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   const [allButton, setallButton] = useState('');
   const [count, setCount] = useState(0)
   const [boxContex, setboxContex] = useState(['', '', '', '', '', '', '', '', '']);
+  const [allDisable, setallDisable] = useState(['', '', '', '', '', '', '', '', '']);
   const box = [0, 1, 2, 3, 4, 5, 6, 7, 8];
   const winer = [
     [0, 1, 2],
@@ -18,56 +19,68 @@ function App() {
     [0, 3, 6]]
   const [newButton, setnewButton] = useState(0)
   const [player, setplayer] = useState("X")
+  
 
-  const OnClickBox = (item, setDisabled) => {
-    let BoxId = item.target.id;
-    // console.log(BoxId)
-    // console.log(count)
+  const OnClickBox = (item) => {
+    const BoxId = item.target.id;
+    if (boxContex[BoxId] || newButton === 1) return;
 
-    if (player === "X") {
-      item.target.innerText = "X"
-      boxContex[BoxId] = 'X'
-      setplayer("O")
-      item.disabled = true
-    } else {
-      item.target.innerText = "O"
-      boxContex[BoxId] = 'O'
-      setplayer("X")
+    const newContext = [...boxContex];
+    newContext[BoxId] = player;
+    setboxContex(newContext);
+
+    const newDisable = [...allDisable];
+    newDisable[BoxId] = true;
+    setallDisable(newDisable);
+
+    const newCount = count + 1;
+    setCount(newCount);
+
+    for (let i = 0; i < winer.length; i++) {
+      const [a, b, c] = winer[i];
+      if (newContext[a] && newContext[a] === newContext[b] && newContext[b] === newContext[c]) {
+        console.log(`Winner: ${newContext[a]}`);
+        setallButton('1');
+        setnewButton(1);
+        return;
+      }
     }
-    // console.log(boxContex)
-    console.log(count)
-    let z = count + 1;
-    setCount(z)
-    console.log(count)
-    setDisabled(1)
+
+    if (newCount === 9) {
+      console.log("Draw");
+      setnewButton(1);
+    }
+
+    setplayer(player === "X" ? "O" : "X");
+  };
+
+
+  const newGame = () => {
+    setallButton('')
+    setnewButton(0)
+    setCount(0);
     let i
-    for (i = 0; i < 8; i++) {
-      let a = winer[i][0]
-      let b = winer[i][1]
-      let c = winer[i][2]
-      if (boxContex[a] && boxContex[a] === boxContex[b] && boxContex[b] === boxContex[c]) {
-        console.log(boxContex[a])
-        // break
-        setallButton('1')
-        setnewButton(1)
-        return 1
-      }
-      if (count === 9) {
-        setnewButton(1)
-        console.log("Drow", count)
-      }
+    for (i = 0; i < 9; i++) {
+      boxContex[i] = ''
+      allDisable[i] = ''
     }
-
   }
 
   return (<center>
     <div className={styles.display}>
       <div className={styles.box}>
-        {box.map((item) => <Box key={item} item={item} allButton = {allButton} OnClickBox={OnClickBox}></Box>)}
+        {box.map((item) => <Box
+          key={item}
+          item={item}
+          disabled={allDisable[item]}
+          allButton={allButton}
+          boxItem={boxContex[item]}
+          OnClickBox={OnClickBox}
+        />)}
       </div>
     </div>
-    {newButton===1 && <button className={styles.newbutton}>New Game</button>}
-    </center>
+    {newButton === 1 && <button className={styles.newbutton} onClick={() => newGame()}>New Game</button>}
+  </center>
   )
 }
 
--- src/box.jsx
+++ src/box.jsx
@@ -1,12 +1,11 @@
-import { useState } from "react"
 import styles from "./box.module.css"
 
-function Box({ item, allButton, OnClickBox }) {
-    const [Disabled,setDisabled] = useState(0)
-
+function Box({ item, OnClickBox, allButton, disabled,boxItem }) {
+    
+    
     return <>
-        <button disabled={Disabled || allButton} id={item} className={styles.boxx} onClick={(event) => OnClickBox(event,setDisabled)}>
-            
+        <button disabled={disabled || allButton} id={item} className={styles.boxx} onClick={(event) => OnClickBox(event)}>
+            {boxItem}
         </button>
     </>
 }
