8c7d092c8fb844d997d09c412501cf92ffe79eb6
Chinmoy Mandal <chinmoymandal925807@gmail.com>
2025-08-11 00:08:41 +0530

    add game icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import styles from './App.module.css'
 import Box from './box'
 import DispayWin from './DisplayWin';
@@ -6,7 +6,7 @@ import LeaderBord from './LeaderBord';
 
 function App() {
   const [allButton, setallButton] = useState('');
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const [boxContex, setboxContex] = useState(['', '', '', '', '', '', '', '', '']);
   const [allDisable, setallDisable] = useState(['', '', '', '', '', '', '', '', '']);
   const box = [0, 1, 2, 3, 4, 5, 6, 7, 8];
@@ -23,6 +23,46 @@ function App() {
   const [player, setplayer] = useState("X")
   const [gameResult, setgameResult] = useState('');
 
+  const priveasData = JSON.parse(localStorage.getItem("tic-tac-toi")) || {};
+  
+  const [leaderboard, setleaderboard] = useState({
+        1: priveasData[1] ? priveasData[1] : 0,
+        2: priveasData[2] ? priveasData[2] : 0
+  });
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("tic-tac-toi")) || {};
+    data[1] = leaderboard[1];
+    data[2] = leaderboard[2];
+    localStorage.setItem("tic-tac-toi", JSON.stringify(data));
+  }, [leaderboard]);
+
+  function ResetGame() {
+    const isConfirmed = confirm("Are you sure you want to reset the game?");
+    if (isConfirmed) {
+      console.log("yes");
+      localStorage.removeItem("tic-tac-toi");
+      setleaderboard({ 1: 0, 2: 0 });
+    }
+  }
+
+  const leaderboradUpdate = (event) => {
+    console.log(event);
+    if (event === 'O') {
+      setleaderboard(prev => ({
+        ...prev,
+        2: prev[2] + 1
+      }));
+      console.log("o");
+    } else if (event === 'X') {
+      setleaderboard(prev => ({
+        ...prev,
+        1: prev[1] + 1
+      }));
+    }
+
+    console.log(leaderboard[1]);
+  }
 
   const OnClickBox = (item) => {
     const BoxId = item.target.id;
@@ -42,12 +82,12 @@ function App() {
     for (let i = 0; i < winer.length; i++) {
       const [a, b, c] = winer[i];
       if (newContext[a] && newContext[a] === newContext[b] && newContext[b] === newContext[c]) {
-        console.log(`Winner: ${newContext[a]}`);
-        setgameResult(newContext[a])
+        setgameResult(`${newContext[a]}`);
         setallButton('1');
         setnewButton(1);
-        xy(newContext[a]);
-        return;
+        console.log(`Winner: ${gameResult}`);
+        leaderboradUpdate(newContext[a]);
+        // return;
       }
     }
 
@@ -60,9 +100,7 @@ function App() {
     setplayer(player === "X" ? "O" : "X");
   };
 
-  const xy = () => {
-    
-  }
+
 
   const newGame = () => {
     setallButton('')
@@ -78,7 +116,7 @@ function App() {
 
   return (<center>
     <div className={styles.tital}>Tic Tac Toi</div>
-    <LeaderBord xy={xy} />
+    <LeaderBord result={leaderboard} />
     <div className={styles.display}>
       <div className={styles.box}>
         {box.map((item) => <Box
@@ -92,7 +130,8 @@ function App() {
       </div>
     </div>
     {gameResult.length >= 1 && <DispayWin gameResult={gameResult} />}
-    {newButton === 1 && <button className={styles.newbutton} onClick={() => newGame()}>New Game</button>}
+    <button className={styles.newbutton} onClick={() => newGame()}>New Game</button>
+    <button className={styles.resetbutton} onClick={() => ResetGame()}>Reset Game</button>
   </center>
   )
 }
--- src/LeaderBord.jsx
+++ src/LeaderBord.jsx
@@ -1,21 +1,23 @@
 import styles from './LeaderBord.module.css';
-import { useState } from 'react'
 
-
-function LeaderBord({ xy }) {
-    const [player_1,setplayer_1] = useState(0);
-    let player_2 = 0;
-    if (xy) setplayer_1(player_1+1);
+// setplayer_1(player_1 + 1)
+function LeaderBord({ result }) {
+    // const [player_1, setplayer_1] = useState(0);
+    
+    
+    // const [player_2, setplayer_2] = useState(0);
+        // console.log(result);
+        // setplayer_2( result==='O' && player_2 + 1);
     return <>
         
         <div className={styles.leaderboard}>
             <div className={styles.box}>
                 <div>Player_X</div>
-                <div>{ player_1}</div>
+                <div>{result[1]}</div>
             </div>
             <div className={styles.box}>
                 <div>Player_O</div>
-                <div>{player_2}</div>
+                <div>{result[2]}</div>
             </div>
         </div>
     </>
